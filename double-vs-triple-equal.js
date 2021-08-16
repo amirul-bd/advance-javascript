73102dd91123c48e3a3fb48f8954ac8b756e379b
amirul-bd <mdamirul655@gmail.com>
2021-08-16 17:37:40 +0600

    equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,14 @@
+const num1 = 2;
+const num2 = '2';
+
+// === dile equal check korbe equal & type;
+
+
+// 0 = false;
+// 1 = true;
+
+if (num1 === num2) {
+    console.log("true");
+} else {
+    console.log('false');
+}
