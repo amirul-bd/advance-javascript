21173e603e03c630d0c294fcd77565a1fcf4a4c0
amirul-bd <mdamirul655@gmail.com>
2021-08-16 18:07:34 +0600

    map filter

---
+++ map-filter.js
@@ -0,0 +1,37 @@
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const output = [];
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result)
+// }
+// console.log(output);
+
+
+
+/* function square(element) {
+      return element * element;
+}*/
+// const square = element => element * element;
+// const square = x => x * x;
+
+//index position bolbe
+//array pura array tai dibe
+// numbers.map(function(element, index, array){
+
+// const result = numbers.map(function(element){
+//     return element * element;
+//     // console.log({element}, {index}, {array}); 
+// })
+
+
+
+// const result = numbers.map(x => x * x )
+
+
+
+
+const result = numbers.filter(x => x > 5)
+
+console.log(result);
+
