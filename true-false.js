e1be436cfa95fe8ea63bf974443be85ea4d64195
amirul-bd <mdamirul655@gmail.com>
2021-08-16 17:10:48 +0600

    true false file added

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//falsy
+//0
+//"",''
+//null
+//undefined
+//NaN
+//false
+
+//truthy 
+//"0", " ", [], {}
+
+let name = {};
+
+if (name) {
+    console.log("Its true ");
+} else {
+    console.log("Its false");
+}
