ed623093e355faeda98812f67097236237211706
amirul-bd <mdamirul655@gmail.com>
2021-08-16 17:31:21 +0600

    null file push

---
+++ null-undefined.js
@@ -0,0 +1,12 @@
+//undefined List
+//let name;
+
+function add (num1 , num2){
+    console.log(num1 + num2);
+}
+const result = add(15,85)
+console.log(result);
+
+//i think jeta defined kora hoi nai oitai undefined
+
+//null hooce set korte hoi
